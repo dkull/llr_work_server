2ed09c2a2f151f6a44a5d91e7d60dec2cefb73d2
Tanel Liiv <tanel.liiv@defendec.com>
2020-05-30 10:50:53 +0300

    Lots of fixes

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import datetime
 import zerorpc
 import subprocess
 import os
@@ -5,79 +6,154 @@ import sys
 import time
 import socket
 
-print("version: 2020-03-10 13:51")
+client_name = socket.gethostname()
 
+print("version: 2020-04-14 18:21")
+print(sys.argv)
+
+f = open("/proc/cpuinfo", "r")
+lines = f.read().split("\n")
+f.close()
+cpus = list(filter(lambda x: "CPU" in x, lines))
+print("{} x {}".format(len(cpus), cpus[0].split(":")[1]))
+
+if sys.argv[1] == "-f":
+    THREADS = 2
+    HOST = "<host>:<port>"
+else:
+    THREADS = int(sys.argv[1])
+    HOST = sys.argv[2]
+    if len(sys.argv) >= 4:
+        client_name = sys.argv[3]
+
+print("hostname {}".format(client_name))
+
+is_cuda = False
 if "linux" in sys.platform:
-    EXE_PATH = "./llr64"
+    if os.path.isfile("./llrCUDA"):
+        is_cuda = True
+        EXE_PATH = "./llrCUDA"
+    else:
+        EXE_PATH = "./llr64"
 else:
     EXE_PATH = "./cllr64.exe"
-THREADS = int(sys.argv[1])
-HOST = sys.argv[2]
 
-client_name = socket.gethostname()
-connect_to = "tcp://{}".format(HOST)
 
-c = zerorpc.Client()
-c.connect(connect_to)
+def run(server, hostname, threads):
+    print("running...")
 
-while True:
-    time.sleep(1)
-    work_threads = THREADS
-    try:
-        f = open("llr.ini", 'r')
-        all_done = "WorkDone=1" in f.read()
-    except:
-        all_done = True
+    connect_to = "tcp://{}".format(server)
+    c = zerorpc.Client()
+    c.connect(connect_to)
 
-    if all_done:
+    use_threads = threads
+    while True:
+        time.sleep(1)
+        now = datetime.datetime.now().__str__()[:22]
         try:
-            os.remove("lresults.txt")
-            os.remove("llr.ini")
-            os.remove('work.npg')
+            f = open("llr.ini", 'r')
+            all_done = "WorkDone=1" in f.read()
         except:
-            pass
+            all_done = True
 
-        try:
-            work = c.get_work(client_name)
-        except:
-            print('get_work failed')
-            time.sleep(10)
-            continue
-
-        print(work)
-        if len(work) == 2:
-            work, work_threads = work[0], work[1]
-
-        if work:
-            f = open('work.npg', 'w')
-            f.write(work[0] + "\n")
-            f.write(" ".join([str(work[1]), str(work[2])]))
-            f.close()
-        else:
-            time.sleep(5)
-            continue
-
-    cmd = [EXE_PATH, '-d', '-t{}'.format(work_threads), 'work.npg']
-    proc = subprocess.Popen(cmd)
-    proc.wait()
-
-    if os.path.isfile('lresults.txt'):
-        work_file = open("work.npg", "r")
-        work_lines = work_file.readlines()
-        header = work_lines[0].strip()
-        k, n = work_lines[1].split(" ")
-        k, n = int(k), int(n)
-        work = (header, k, n)
-
-        result = open('lresults.txt', 'r')
-        res = result.read()
-        result.close()
-
-        while True:
+        if all_done:
             try:
-                c.report_work(client_name, work, res)
-                break
+                os.remove("lresults.txt")
+                os.remove("llr.ini")
+                os.remove('work.npg')
             except:
-                print("failed to report work: ", work, res)
+                pass
+
+            try:
+                work = c.get_work(hostname)
+            except:
+                print(f'{now}: get_work failed')
                 time.sleep(10)
                 continue
+
+            if isinstance(work, dict):
+                got_state_file = 'state' in work and work['state'] is not None
+                if got_state_file:
+                    state_file_name, state_file_data = work['state']
+                    f = open(state_file_name, 'wb')
+                    f.write(state_file_data)
+                    f.close()
+                work = work['work']
+                print(f"{now}: got resumable work from server {work} state? {got_state_file}")
+            else:
+                print(f"{now}: {work}")
+                if len(work) == 2:
+                    work, use_threads = work[0], work[1]
+                else:
+                    use_threads = threads
+
+            if work:
+                f = open('work.npg', 'w')
+                f.write(work[0] + "\n")
+                f.write(" ".join([str(work[1]), str(work[2])]))
+                f.close()
+            else:
+                time.sleep(5)
+                continue
+        else:
+            print(f"{now}: resuming from local offline files...")
+
+        if is_cuda:
+            cmd = [EXE_PATH, '-d', 'work.npg']
+        else:
+            cmd = [EXE_PATH, '-d', '-t{}'.format(use_threads), '-oDiskWriteTime=1', 'work.npg']
+
+        proc = subprocess.Popen(cmd)
+        i = 0
+        while True:
+            time.sleep(10)
+            now = datetime.datetime.now().__str__()[:22]
+            if i % 6 == 0:
+                files = os.listdir("./")
+                for f in files:
+                    if f.startswith("z"):
+                        try:
+                            _f = open(f, 'rb')
+                            data = _f.read()
+                            _f.close()
+                        except:
+                            break
+                        try:
+                            c.report_progress(hostname, f, data)
+                        except Exception as e:
+                            print(f"{now}: failed to report progress: {e}")
+            retcode = proc.poll()
+            i = (i + 1) % 6
+            if retcode is not None:
+                break
+        proc.wait()
+
+        if os.path.isfile('lresults.txt'):
+            work_file = open("work.npg", "r")
+            work_lines = work_file.readlines()
+            header = work_lines[0].strip()
+            k, n = work_lines[1].split(" ")
+            k, n = int(k), int(n)
+            work = (header, k, n)
+
+            result = open('lresults.txt', 'r')
+            res = result.read()
+            result.close()
+
+            while True:
+                now = datetime.datetime.now().__str__()[:22]
+                try:
+                    if not is_cuda:
+                        c.report_work(hostname, work, res)
+                        print(f"{now}: success report work: ", work, res)
+                    break
+                except:
+                    print(f"{now}: failed to report work: ", work, res)
+                    time.sleep(2)
+                    continue
+
+def mymain():
+    run(HOST, client_name, THREADS)
+
+if __name__ == '__main__':
+    mymain()
--- server.py
+++ server.py
@@ -1,7 +1,9 @@
+import gc
 import datetime
 from time import time
 import zerorpc
 import os
+import sys
 
 WORK_FILES_DIR = "work"
 RESULT_FILES_DIR = "results"
@@ -28,6 +30,20 @@ def scheduled_threads(client_name):
 
     return None
 
+def read_clients_from_file():
+    f = open('servers.txt', 'r')
+    data = f.readlines()
+    f.close()
+    clean_names = []
+    for line in data:
+        name = line.strip()
+        if '#' in line:
+            name = line[:line.index('#')].strip()
+        if len(name) <= 1:
+            continue
+        clean_names.append(name)
+    return clean_names
+
 
 def work_to_name(work):
     sieve_p = work[0].index(":")
@@ -37,10 +53,21 @@ def work_to_name(work):
     return line
 
 
-def get_next_work():
+def get_next_work(client_name):
     work_files = os.listdir(WORK_FILES_DIR)
     done_files = set(os.listdir(RESULT_FILES_DIR))
     in_progress = set(os.listdir(IN_PROGRESS_DIR))
+
+    for in_prog in in_progress:
+        f = open(os.path.join(IN_PROGRESS_DIR, in_prog), "r")
+        data = f.readlines()[0]
+        f.close()
+        if client_name == data.strip():
+            header = "1111111111:M:1:2:258"
+            # 0_M_1_2_25_193_3083303
+            _,_,_,_,_,k,n = in_prog.split("_")
+            work = (header, int(k), int(n)), True
+            return work
 
     all_work = []
     for work_file in work_files:
@@ -61,6 +88,9 @@ def get_next_work():
             all_work,
         )
     )
+
+    # sort by n
+    available_work.sort(key=lambda x: x[2])
 
     active_k = available_work[0][1]
 
@@ -94,7 +124,7 @@ def get_next_work():
 
     if available_work:
         first_available = available_work[0]
-        return first_available
+        return first_available, False
     else:
         None
 
@@ -104,27 +134,56 @@ def print_stats(clients):
     jobs_in_hour = 0.0
     missing_jobs_in_hour = 0.0
     active_clients = 0
-    for (name, client) in clients.items():
-        work_rate = 3600.0 / client.last_work_duration
-        if client.last_seen + (3600 * 6) < now:
-            missing_jobs_in_hour += work_rate
-            print(
-                "client {} not seen for a while now (missing out on {}/h)".format(
-                    client.name
-                )
-            )
-            continue
-        active_clients += 1
-        jobs_in_hour += work_rate
-    print(
-        "{}/{} active clients process {:.3f}/h {:.2f}/24h, missing out on {}/h".format(
-            active_clients,
-            len(clients),
-            jobs_in_hour,
-            jobs_in_hour * 24,
-            missing_jobs_in_hour,
-        )
-    )
+    reporting_clients = 0
+
+    static_client_names = read_clients_from_file()
+    for client_name in static_client_names:
+        client = clients.get(client_name, None)
+
+        # set defaults
+        work_rate = -1.0
+        last_reported = -1.0
+        last_completion = -1.0
+
+        # calc stuff
+        if client:
+            if client.last_work_duration != -1:
+                work_rate = 3600.0 / client.last_work_duration
+            if client.last_completed != -1:
+                last_completion = now - client.last_completed
+            if client.last_reported != -1:
+                last_reported = now - client.last_reported
+
+        completion_time_thresh = 3600
+        if client and client.last_work_duration != -1:
+            completion_time_thresh = client.last_work_duration * 1.2
+        last_completion_recently = last_completion >= 0 and last_completion < completion_time_thresh
+        last_report_recently = last_reported >= 0 and last_reported < 60 * 5
+        if last_report_recently or (last_completion_recently and (last_report_recently or last_reported == -1)):
+            active_clients += 1
+            if work_rate != -1.0:
+                reporting_clients += 1
+                jobs_in_hour += work_rate
+
+        # format stuff for printing
+        report_status = "{}".format("R" if not last_report_recently else " ")
+        if report_status == "R" and last_completion_recently:
+            report_status = "."
+        completion_status = "{}".format("C" if not last_completion_recently else " ")
+        if completion_status == "C" and last_report_recently:
+            completion_status = "."
+
+        last_report_repr = "{:>4.0f}".format(last_reported) if last_reported != -1 else "    "
+        last_completed_repr = "{:>4.0f} [{:.2f}]".format(last_completion, last_completion/client.last_work_duration) if client and last_completion != -1 else "    "
+        rate_repr = "{:>5.1f}".format(work_rate) if work_rate != -1 else "      "
+
+        print("{}{} client {:>16} last completed {} reported {} rate {}".format(
+            report_status, completion_status, client_name, last_completed_repr, last_report_repr, rate_repr))
+
+    for client_name in clients.keys():
+        if client_name not in static_client_names:
+            print("have unseen client: {}".format(client_name))
+    print("active static clients: {}/{}/{} rates: {:.2f}/h {:.2f}/24h".format(reporting_clients, active_clients, len(static_client_names), jobs_in_hour, jobs_in_hour*24))
 
 
 def get_or_create_client(clients, name):
@@ -138,9 +197,13 @@ def get_or_create_client(clients, name):
 class Client:
     def __init__(self, name):
         self.name = name
-        self.last_seen = 0
-        self.last_work_duration = 1.0  # to not get division by zero
-
+        self.last_got_work = -1
+        self.last_completed = -1
+        self.last_reported = -1
+        self.last_work_duration = -1
+        self.state_file_name = None
+        self.state_file_data = None
+        self.state_file_time = None
 
 class RPCServer(object):
     def __init__(self):
@@ -148,45 +211,84 @@ class RPCServer(object):
         self.anomaly_seen = False
 
     def get_work(self, client_name):
+        # python2
+        if isinstance(client_name, bytes):
+            client_name = client_name.decode('utf-8')
+
         if self.anomaly_seen:
-            print("\n-> get work [{}] __ANOMALY_SEEN__".format(client_name))
+            print("\n-> get work [{}] ########## ANOMALY_SEEN ###########".format(client_name))
         else:
             print("\n-> get work [{}]".format(client_name))
 
-        work = get_next_work()
+        client = get_or_create_client(self.clients, client_name)
+        client.last_got_work = time()
+
+        work, resume = get_next_work(client_name)
         if not work:
             print("no work to give")
+            sys.stdout.flush()
             return None
+        if resume:
+            client = self.clients.get(client_name, None)
+            if client.state_file_name is not None:
+                print("giving resume with state for {} {}".format(client_name, work))
+                (when, resume_name, resume_file) = client.state_file_time, client.state_file_name, client.state_file_data
+                sys.stdout.flush()
+                return {'work': work, 'state': (resume_name, resume_file)}
+            else:
+                print("giving resume without state for {} {}".format(client_name, work))
+                sys.stdout.flush()
+                return {'work': work}
 
         work_name = work_to_name(work)
-
         path = os.path.join(IN_PROGRESS_DIR, work_name)
         f = open(path, "x")
+
         try:
-            # python2
-            if isinstance(client_name, bytes):
-                client_name = client_name.decode('utf-8')
             f.write(client_name)
             f.close()
 
-            print_stats(self.clients)
+            try:
+                print_stats(self.clients)
+            except Exception as e:
+                print("stats exception: {}".format(e))
+
             threads = scheduled_threads(client_name)
 
+            giving_work = "giving work {} to {}".format(work, client_name)
             if threads:
-                print("giving work {} to {} [threads {}]".format(work, client_name, threads))
-            else:
-                print("giving work {} to {}".format(work, client_name))
+                giving_work += " [threads {}]".format(threads)
+            print(giving_work)
 
             if threads:
+                sys.stdout.flush()
                 return [work, threads]
             else:
+                sys.stdout.flush()
                 return work
         except Exception as e:
             f.close()
             print("removing file due to exception: ", path, e)
             os.remove(path)
 
+    def report_progress(self, client_name, state_file_name, state_file):
+        # python2
+        if isinstance(client_name, bytes):
+            client_name = client_name.decode('utf-8')
+        #print("\n-> report progress [{}] {}".format(client_name, datetime.datetime.utcnow().isoformat()))
+        now = time()
+        client = get_or_create_client(self.clients, client_name)
+        client.state_file_name = state_file_name
+        client.state_file_data = state_file
+        client.last_reported = now
+        gc.collect()
+        sys.stdout.flush()
+
     def report_work(self, client_name, work, result):
+        # python2
+        if isinstance(client_name, bytes):
+            client_name = client_name.decode('utf-8')
+
         result = result.strip()
 
         # Python2
@@ -199,7 +301,10 @@ class RPCServer(object):
         work_name = work_to_name(work)
 
         client = get_or_create_client(self.clients, client_name)
-        client.last_seen = time()
+        client.last_completed = time()
+        client.state_file_name = None
+        client.state_file_data = None
+        client.state_file_time = None
         try:
             duration = float(result.split(" ")[-2])
             client.last_work_duration = duration
@@ -214,6 +319,7 @@ class RPCServer(object):
         result_exists = os.path.isfile(os.path.join(RESULT_FILES_DIR, work_name))
         if result_exists:
             print("{} reported already done work {}".format(client_name, work_name))
+            sys.stdout.flush()
             return True
 
         os.remove(os.path.join(IN_PROGRESS_DIR, work_name))
@@ -221,13 +327,18 @@ class RPCServer(object):
         f.write(result)
         f.close()
 
+        gc.collect()
+
+        sys.stdout.flush()
         return True
 
 
-s = zerorpc.Server(RPCServer())
-s.bind("tcp://0.0.0.0:8830")
-try:
-    s.run()
-except:
-    # don't throw on Ctrl-c
-    pass
+while True:
+    try:
+        s = zerorpc.Server(RPCServer())
+        s.bind("tcp://0.0.0.0:8830")
+        s.run()
+    except Exception as e:
+        # don't throw on Ctrl-c
+        print(e)
+print("EXITING???")
